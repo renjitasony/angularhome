99f47afe7779bd5e17fccb1f09ea1b362d41b1af
renjitasony <renjitasony@gmail.com>
2019-08-25 22:00:25 +0530

    deplo

--- firstpro/src/app/app.module.ts
+++ firstpro/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {Routes,RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { CartComponent } from './cart/cart.component';
 import { DemoComponent } from './demo/demo.component';
+import { NamedisplayComponent } from './namedisplay/namedisplay.component';
+import { from } from 'rxjs';
+import { CrudComponent } from './crud/crud.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
-    DemoComponent
+    DemoComponent,
+    NamedisplayComponent,
+    CrudComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule,RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- firstpro/src/app/cart/cart.component.ts
+++ firstpro/src/app/cart/cart.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  products=[];
+  cart=[];
+  view:Boolean=true;
+  constructor() { 
+    this.products= [{name:"Printer",price:8000,company:"Laserjet"},
+    {name:"Scanner",price:12000,company:"HP"},
+    {name:"Keyboard",price:1800,company:"Lenovo"}
+   ]
+  }
 
   ngOnInit() {
   }
+  public Add(i){
+    
+    let data = this.products[i];
+   //let data = this.products.splice(i,1);
+   this.cart.push(data);
+ }
+ public Remove(i){
+   this.cart.splice(i,1);    
+ }
+ public showCart(show){
+   this.view = show;
+ }
 
 }
